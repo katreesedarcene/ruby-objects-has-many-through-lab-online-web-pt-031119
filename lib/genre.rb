8e4bd35048ce702dd8fde77efae5e27eaedd5732
Kat B <katreesebarnes@gmail.com>
2019-04-12 23:15:56 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,10 +1,10 @@
 class Genre 
   @@all = []
-  attr_accessor :name, :artists
+  attr_accessor :name
   
   def initialize(name)
     @name = name 
-    @artists = artists
+   
     @@all << self 
   end
   def self. all 
@@ -14,6 +14,8 @@ def songs
       Song.all { |song| song.genre == self }
 
 end 
+def artists
+end
   
   
 end
