ae6566e61842bab791b7cdfeffbd553267799a70
Kat B <katreesebarnes@gmail.com>
2019-04-12 23:15:10 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,10 @@
 class Genre 
   @@all = []
-  attr_accessor :name 
+  attr_accessor :name, :artists
   
   def initialize(name)
     @name = name 
+    @artists = artists
     @@all << self 
   end
   def self. all 
