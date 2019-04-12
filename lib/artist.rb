9bf835f60617525f62396dce5d1d4bd6d03c151e
Kat B <katreesebarnes@gmail.com>
2019-04-12 23:08:21 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require "pry"
+class Artist 
+  @@all = []
+  attr_accessor :name 
+  
+  def initialize(name) 
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all 
+  end 
+  def new_song(name, genre) 
+    Song.new(name, self, genre)
+  end
+  def  songs
+    Song.all.select do |s|
+      s.artist == self
+    end
+  end
+  def genres 
+    Genre.all.select do |s| 
+      s[value]
+      
+    end
+  end
+  
+end
