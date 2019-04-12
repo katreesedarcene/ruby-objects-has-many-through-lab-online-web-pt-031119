db6cf7d26170c1bd3a31658dd6f251276ef94cb7
Kat B <katreesebarnes@gmail.com>
2019-04-12 23:31:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,8 +12,9 @@ class Patient
      Appointment.new(self, doctor, date)
   end
   def appointments
-    Appointment.all.select do  |appointment| appointment.patient == self 
-    
+    Appointment.all.select do  |appointment| 
+    appointment.patient == self 
+  end
   end
   
 end
